a28515652b0c44fd111be4e2a6a573ec4ad6d3cf
Svat-dev <svatoslavgetun@gmail.com>
2026-06-07 15:30:41 +0300

    feat: Allow registration token generation with user name details

--- internal/handlers/auth/generate_token.go
+++ internal/handlers/auth/generate_token.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -11,10 +12,29 @@ import (
 	"cascade/pkg/filter"
 )
 
+type GenRegTokenDto struct {
+	Name     string  `json:"name" binding:"required,alpha"`
+	Surname  string  `json:"surname" binding:"required,alpha"`
+	LastName *string `json:"last_name" binding:"alpha"`
+}
+
 func GenerateRegisterToken(c *gin.Context) {
+	var dto GenRegTokenDto
+	if err := c.ShouldBind(&dto); err != nil {
+		filter.Error(c, filter.ErrorParams{
+			Status:  http.StatusBadRequest,
+			Message: "Неверно введены значения!",
+			Cause:   err.Error(),
+		})
+		return
+	}
+
 	user := models.User{
-		ID:   uuid.New(),
-		Role: models.RoleRegular,
+		ID:       uuid.New(),
+		Role:     models.RoleRegular,
+		Name:     dto.Name,
+		Surname:  dto.Surname,
+		LastName: dto.LastName,
 	}
 
 	config.DB.Model(&models.User{}).Create(&user)
