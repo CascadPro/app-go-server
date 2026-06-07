f3a34704b48a682d30a21762d82c9421e657c860
Svat-dev <svatoslavgetun@gmail.com>
2026-06-07 15:30:41 +0300

    feat: Update GetMyAccount to include new user profile fields

--- internal/handlers/account/get_my_account.go
+++ internal/handlers/account/get_my_account.go
@@ -1,10 +1,11 @@
 package account
 
 import (
+	"net/http"
+
 	"cascade/config"
 	"cascade/internal/models"
 	"cascade/pkg/filter"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,7 +21,7 @@ func GetMyAccount(c *gin.Context) {
 
 	var user models.User
 
-	err := config.DB.Select("id", "role", "email", "created_at", "updated_at").First(&user, "id = ?", userID).Error
+	err := config.DB.Select("id", "role", "email", "name", "surname", "avatar", "created_at", "updated_at").First(&user, "id = ?", userID).Error
 	if err != nil {
 		filter.Error(c, filter.ErrorParams{Status: http.StatusNotFound, Message: "Пользователь не был найден!"})
 		return
